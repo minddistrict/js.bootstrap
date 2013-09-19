1930a3a87878426578f8b484bfdc763500f85192
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2013-09-19 13:47:25 +0200

    Update to bootstrap v3.0.0. Update tests to fanstatic 1.0a3.

--- js/bootstrap/__init__.py
+++ js/bootstrap/__init__.py
@@ -5,10 +5,10 @@ library = Library('bootstrap', 'resources')
 
 bootstrap_css = Resource(library, 'css/bootstrap.css',
                          minified='css/bootstrap.min.css')
-bootstrap_responsive_css = Resource(library, 'css/bootstrap-responsive.css',
-                                    minified='css/bootstrap-responsive.min.css',
-                                    depends=[bootstrap_css, ],
-                                    dont_bundle=True)
+bootstrap_theme_css = Resource(library, 'css/bootstrap-theme.css',
+                               minified='css/bootstrap-theme.min.css',
+                               depends=[bootstrap_css, ],
+                               dont_bundle=True)
 bootstrap_js = Resource(library, 'js/bootstrap.js',
                         minified='js/bootstrap.min.js',
                         depends=[jquery, ])
